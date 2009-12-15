ef820c0b591ddf6af3ca5fa8d92305a0a919b7cc
schoenborn <schoenborn@caa2ac01-1b1e-4351-a100-d44f7e30510d>
2009-12-15 21:02:46 +0000

    added parse method for Locale strings

--- src/main/java/de/cosmocode/commons/Locales.java
+++ src/main/java/de/cosmocode/commons/Locales.java
@@ -1,6 +1,9 @@
 package de.cosmocode.commons;
 
 import java.util.Locale;
+import java.util.regex.Matcher;
+
+import com.google.common.base.Preconditions;
 
 /**
  * Utility class providing constant and
@@ -18,6 +21,28 @@ public final class Locales {
      */
     private Locales() {
         
+    }
+    
+    /**
+     * Parses a string into a {@link Locale}.
+     * 
+     * @param value the locale string
+     * @return a new {@link Locale} parsed from value
+     * @throws NullPointerException if value is null
+     * @throws IllegalArgumentException if value is no valid locale
+     */
+    public static Locale parse(String value) {
+        Preconditions.checkNotNull(value, "Value");
+        final Matcher matcher = Patterns.LOCALE.matcher(value);
+        if (matcher.matches()) {
+            final String language = Strings.defaultIfBlank(matcher.group(1), "");
+            final String country = Strings.defaultIfBlank(matcher.group(2), "");
+            final String variant = Strings.defaultIfBlank(matcher.group(3), "");
+            return new Locale(language, country, variant);
+        } else {
+            final String message = String.format("%s does not match %", value, Patterns.LOCALE.pattern());
+            throw new IllegalArgumentException(message);
+        }
     }
     
 }
