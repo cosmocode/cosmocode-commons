9b2d0ea68a59a842628bfd560877e410a110c53c
schoenborn <schoenborn@caa2ac01-1b1e-4351-a100-d44f7e30510d>
2010-04-21 14:49:59 +0000

    Calendars.toBeginningOfTheDay(..) now set the milliseconds field also to 0

--- src/main/java/de/cosmocode/commons/Calendars.java
+++ src/main/java/de/cosmocode/commons/Calendars.java
@@ -43,6 +43,7 @@ public final class Calendars {
         calendar.set(Calendar.HOUR_OF_DAY, 0);
         calendar.set(Calendar.MINUTE, 0);
         calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
     }
     
     /**
