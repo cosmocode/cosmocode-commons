08f82096cb60558a71ed858ed8e45aaa4995708d
schoenborn <schoenborn@caa2ac01-1b1e-4351-a100-d44f7e30510d>
2010-09-02 19:03:04 +0000

    added first draft of Decimal- and BinaryUnit

---
+++ src/main/java/de/cosmocode/commons/BinaryUnit.java
@@ -0,0 +1,30 @@
+package de.cosmocode.commons;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * A unit enum similiar to {@link TimeUnit} for
+ * binary units used for storage sizes.
+ *
+ * @since 1.14
+ * @author Willi Schoenborn
+ */
+enum BinaryUnit {
+
+    KIBI,
+    
+    MIBI,
+    
+    GIBI,
+    
+    TEBI,
+    
+    PEBI,
+    
+    EXIBI,
+    
+    ZEBI,
+    
+    YOBI;
+    
+}
---
+++ src/main/java/de/cosmocode/commons/DecimalUnit.java
@@ -0,0 +1,30 @@
+package de.cosmocode.commons;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * A unit enum similiar to {@link TimeUnit} for
+ * deciaml prefix units.
+ *
+ * @since 1.14
+ * @author Willi Schoenborn
+ */
+enum DecimalUnit {
+
+   KILO,
+   
+   MEGA,
+   
+   GIGA,
+   
+   TERA,
+   
+   PETA,
+   
+   EXA,
+   
+   ZETTA,
+   
+   YOTTA;
+    
+}
