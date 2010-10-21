105eb777174670a41822950b2a1bf2b517cbc942
schoenborn <schoenborn@caa2ac01-1b1e-4351-a100-d44f7e30510d>
2010-10-21 08:59:07 +0000

    added Joiner for collection elements

--- src/main/java/de/cosmocode/commons/base/Joiners.java
+++ src/main/java/de/cosmocode/commons/base/Joiners.java
@@ -1,4 +1,22 @@
+/**
+ * Copyright 2010 CosmoCode GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package de.cosmocode.commons.base;
+
+import java.util.Collections;
 
 import com.google.common.base.Joiner;
 
@@ -15,6 +33,12 @@ public final class Joiners {
     public static final Joiner BLANK = Joiner.on(" ");
     
     public static final Joiner COMMA = Joiner.on(',');
+    
+    /**
+     * This joiner is especially useful when building string representations
+     * of {@link Collection}s.
+     */
+    public static final Joiner COLLECTION_ELEMENTS = Joiner.on(", ");
     
     private Joiners() {
         
