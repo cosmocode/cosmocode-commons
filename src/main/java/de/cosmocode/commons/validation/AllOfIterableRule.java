5191dc06b0fb06e1f741c0ebcb9c0a6487e96441
Oliver Lorenz <lorenz@cosmocode.de>
2010-12-14 22:33:07 +0100

    added 3 new Rules for iterables (not yet tested), and a TimePeriod interface. All of them are annotated as @Beta and package private (yes, even the interface). The Rules will remain package private, but the TimePeriod will become public once it seems useful enough (and integrates well)

---
+++ src/main/java/de/cosmocode/commons/validation/AllOfIterableRule.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright 2010 CosmoCode GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.cosmocode.commons.validation;
+
+import com.google.common.annotations.Beta;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+/**
+ * Rule implementation that applies a rule to all elements of an Iterable.
+ * Applies if the given rule applies to all of the elements.
+ *
+ * @since 1.21
+ * @author Oliver Lorenz
+ * @param <E> generic type of the Iterable
+ */
+@Beta
+final class AllOfIterableRule<E> extends AbstractRule<Iterable<E>> {
+
+    private final Rule<E> inversedSingleApplicableRule;
+    private final Rule<E> singleApplicableRule;
+
+    AllOfIterableRule(@Nonnull final Rule<E> singleApplicableRule) {
+        this.singleApplicableRule = singleApplicableRule;
+        this.inversedSingleApplicableRule = singleApplicableRule.negate();
+    }
+
+    @Override
+    public boolean apply(@Nullable Iterable<E> input) {
+        if (input == null) {
+            return false;
+        }
+
+        for (final E element : input) {
+            final boolean elementDoesNotApply = inversedSingleApplicableRule.apply(element);
+            if (elementDoesNotApply) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Rules.allOf(" + singleApplicableRule.toString() + ")";
+    }
+
+}
