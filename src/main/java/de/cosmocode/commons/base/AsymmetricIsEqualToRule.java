43eb7707397076f171eb1a4887152e0cb1c8a9b2
Willi Schönborn <schoenborn@cosmocode.de>
2010-12-22 18:42:39 +0100

    added asymmetric equalTo function

---
+++ src/main/java/de/cosmocode/commons/base/AsymmetricIsEqualToRule.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright 2010 CosmoCode GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.cosmocode.commons.base;
+
+import javax.annotation.Nullable;
+
+import com.google.common.base.Preconditions;
+
+import de.cosmocode.commons.validation.AbstractRule;
+import de.cosmocode.commons.validation.Rule;
+import de.cosmocode.commons.validation.Rules;
+
+/**
+ * An asymmetric version of {@link Rules#equalTo(Object)}.
+ *
+ * @since 1.21
+ * @author Willi Schoenborn
+ */
+final class AsymmetricIsEqualToRule extends AbstractRule<Object> {
+
+    private final Object target;
+    
+    AsymmetricIsEqualToRule(Object target) {
+        this.target = Preconditions.checkNotNull(target, "Target");
+    }
+    
+    @Override
+    public boolean apply(@Nullable Object input) {
+        if (input == null) {
+            return false;
+        } else {
+            return target.equals(input) || input.equals(target);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "MoreObjects.asymetricEqualTo(" + target + ")";
+    }
+
+}
---
+++ src/main/java/de/cosmocode/commons/base/MoreObjects.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright 2010 CosmoCode GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.cosmocode.commons.base;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+import javax.annotation.Nullable;
+
+import com.google.common.base.Objects;
+
+import de.cosmocode.commons.validation.Rule;
+import de.cosmocode.commons.validation.Rules;
+
+/**
+ * Static utility class for {@link Object}s, just like {@link Objects}.
+ *
+ * @since 1.21
+ * @author Willi Schoenborn
+ */
+public final class MoreObjects {
+
+    private MoreObjects() {
+        
+    }
+    
+    /**
+     * Creates a rule similiar to {@link Rules#equalTo(Object)} but checks for equality
+     * by comparing target to the given input and vice versa to support
+     * broken equals implementations like {@link Date} and {@link Timestamp}. 
+     *
+     * @since 1.21
+     * @param target the target object
+     * @return an asymmetric equal to rule
+     */
+    public static Rule<Object> asymmetricEqualTo(@Nullable Object target) {
+        return target == null ? Rules.isNull() : new AsymmetricIsEqualToRule(target);
+    }
+    
+}
